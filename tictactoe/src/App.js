db1190d5887a25d85fe9cac21f3f4d3556b49336
JEJU-Ratus <smh1141@naver.com>
2024-01-30 16:57:09 +0900

    feat : 전체적인 틀 세팅

--- tictactoe/src/App.js
+++ tictactoe/src/App.js
@@ -1,7 +1,12 @@
 function App() {
   return (
-    <div>
+    <div className="game">
+      <div className="game-board">
 
+      </div>
+      <div className="game-info">
+        
+      </div>
     </div>
   );
 }
