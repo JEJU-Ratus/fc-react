7954066c1937636c453b770426b1f809b8c453c6
JEJU-Ratus <smh1141@naver.com>
2024-01-31 09:34:50 +0900

    feat : Board, Square 컴포넌트 생성

--- tictactoe/src/App.js
+++ tictactoe/src/App.js
@@ -1,11 +1,13 @@
+import './App.css';
+
 function App() {
   return (
     <div className="game">
       <div className="game-board">
-
+        <p>game-board</p>
       </div>
       <div className="game-info">
-        
+        <p>game-info</p>
       </div>
     </div>
   );
