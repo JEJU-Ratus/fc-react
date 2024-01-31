8376ec192f8b80f08058e3486c17898c2bdd19b1
JEJU-Ratus <smh1141@naver.com>
2024-01-31 10:13:05 +0900

    feat : square 컴포넌트 default 설정 및 board 컴포넌트에서 square 컴포넌트 활용

--- tictactoe/src/components/Square.js
+++ tictactoe/src/components/Square.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-export class Square extends React.Component{
+export default class Square extends React.Component{
   render(){
     return(
       <button className='square'>
