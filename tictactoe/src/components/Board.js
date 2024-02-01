509bc3e820f79c45c4be3a144969a75873f8c4fb
JEJU-Ratus <smh1141@naver.com>
2024-02-01 10:46:03 +0900

    feat : 추가 winner 출력시 및  이미 클릭한 곳은 클릭이 안되게

--- tictactoe/src/components/Board.js
+++ tictactoe/src/components/Board.js
@@ -7,9 +7,40 @@ const Board = () => {
   const [squares, setSquares] = useState(Array(9).fill(null));
   const [xIsNext, setxIsNext] = useState(true)
 
-  const status = `Next Player : ${xIsNext ? 'X' : 'O'}`
+
+  const claculateWinner = (squares) =>{
+    const lines = [
+      [0,1,2],
+      [3,4,5],
+      [6,7,8],
+      [0,3,6],
+      [1,4,7],
+      [2,5,8],
+      [0,4,8],
+      [2,4,6],
+    ]
+    for(let i = 0; i < lines.length; i++){
+      const [a,b,c] = lines[i];
+      if(squares[a] && squares[a] === squares[b] && squares[a] === squares[c]){
+        return squares[a];
+      }
+    }
+    return null;
+  }
+  const winner = claculateWinner(squares);
+  let status;
+
+  if(winner){
+    status = `Winner : ${winner}`;
+  } else{
+    status = `Next Player : ${xIsNext ? 'X' : 'O'}`;
+  }
+  
   const handleClick = (i) => {
     const newSquares = squares.slice();
+    if(claculateWinner(newSquares) ||newSquares[i]){
+      return;
+    }
     newSquares[i] = xIsNext? 'X':'O';
     setxIsNext(prev => !prev)
     setSquares(newSquares);
@@ -20,6 +51,9 @@ const Board = () => {
               value={squares[i]} 
               onClick={() => handleClick(i)} />
   }
+
+
+
   return (
       <div>
         <div className='status'>{status}</div>
